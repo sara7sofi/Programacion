ef4dd57d4389ed8874bbfd1bbf1e347dbbed758c
sara7sofi <sara.arias.7@gmail.com>
2021-10-20 20:12:05 +0200

    Primera actualización

---
+++ Pruebas.java
@@ -0,0 +1,32 @@
+package estructurasDeControl;
+
+import java.util.Scanner;
+
+public class Pruebas {
+    public static void main(String[] args) {
+
+        //ESTRUCTURA CONDICIONAL
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Edad: ");
+        int edad = sc.nextInt();
+        /*Si tiene mas o igual de 18 años, adelante*/
+        //if (edad >= 18) {
+           //System.out.println("Mayor de edad . Adelante");
+        }
+        //System.out.println("Siguiente instrucción tras el if.");
+        //Puedo poner sólo una variable booleana
+
+       //boolean abierta = true;
+        /*if (abierta == true) {
+            //System.out.println("Pasa");
+        //2º caso: if -else (a posibilidades)*/
+
+        /*if (edad >= 18) {
+            //instrucciones true
+            System.out.println("Adelante");
+        } else { //edad <18
+            //instrucciones false
+            System.out.println("No puedes pasar.");*/
+        }
+    //}
+//}
